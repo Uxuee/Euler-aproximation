3a25c9f70e218aa6d2bb11297fa9be2ba4f2a481
Ariadna Uxue <53279566+Uxuee@users.noreply.github.com>
2020-02-20 00:54:49 -0500

    Add files via upload

---
+++ slope_modificar_modulox.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  1 18:06:37 2020
+
+@author: Ariadna
+"""
+
+from modulo import modulo
+
+def slope(xi,yi,q1,q2,x0,y0):
+    r1=((xi-x0)**2+(yi-y0)**2)**0.5
+    r2=((xi+x0)**2+(yi+y0)**2)**0.5
+    
+    Ex=q1*(xi-x0)/(r1**3)+q2*(xi+x0)/(r2**3)
+    Ey=q1*(yi-y0)/(r1**3)+q2*(yi+y0)/(r2**3)
+    E=modulo(Ex,Ey)
+    return Ey/E
